b414b6cbda27e4a4a8c7b599c824624a43d81d89
Mradul-Lakhotiya <mradullakhotiya18@gmail.com>
2025-05-01 17:39:41 +0530

    Modifyed Code

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import numpy as np
 app = Flask(__name__)
 
 # Load pickled data
-popular_df = pickle.load(open('popular.pkl', 'rb'))
-pt = pickle.load(open('pt.pkl', 'rb'))
-books = pickle.load(open('books.pkl', 'rb'))
-similarity_scores = pickle.load(open('similarity_scores.pkl', 'rb'))
+popular_df = pickle.load(open('./Models/popular.pkl', 'rb'))
+pt = pickle.load(open('./Models/pt.pkl', 'rb'))
+books = pickle.load(open('./Models/books.pkl', 'rb'))
+similarity_scores = pickle.load(open('./Models/similarity_scores.pkl', 'rb'))
 
 @app.route('/')
 def home():
